324ac82311372d401f3efab93f445cd093382767
YenNhi2000 <87085694+YenNhi2000@users.noreply.github.com>
2021-07-19 11:07:22 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "db_laptoponline";
+$servername = "MYSQL5045.site4now.net";
+$username = "a771dd_laptop";
+$password = "1q2w3e4r";
+$db = "db_a771dd_laptop";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
